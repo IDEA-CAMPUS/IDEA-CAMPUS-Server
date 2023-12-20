227f8f83bc536e204bbf3650653465ffca071a46
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2023-12-21 00:51:27 +0900

    Feat: 동아리/학회 글 등록 기능 구현

--- src/main/java/depth/main/ideac/domain/club_post/application/ClubPostService.java
+++ src/main/java/depth/main/ideac/domain/club_post/application/ClubPostService.java
@@ -1,11 +1,44 @@
 package depth.main.ideac.domain.club_post.application;
 
+import depth.main.ideac.domain.club_post.domain.ClubPost;
+import depth.main.ideac.domain.club_post.domain.repository.ClubPostRepository;
+import depth.main.ideac.domain.club_post.dto.ClubPostDetailRes;
+import depth.main.ideac.domain.club_post.dto.ClubPostReq;
+import depth.main.ideac.domain.club_post.dto.ClubPostRes;
+import depth.main.ideac.domain.user.domain.User;
+import depth.main.ideac.domain.user.domain.repository.UserRepository;
+import depth.main.ideac.global.error.DefaultException;
+import depth.main.ideac.global.payload.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class ClubPostService {
+
+    private final UserRepository userRepository;
+    private final ClubPostRepository clubPostRepository;
+    
+    // 전체 조회
+    // public List<ClubPostRes> getIdea()
+    // 비회원, 회원 둘 다 가능하게
+    // 페이징
+    
+    // 상세 조회
+
+    public void createClubPost(Long userId, ClubPostReq clubPostReq) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new DefaultException(ErrorCode.INVALID_PARAMETER));
+
+        ClubPost clubPost = ClubPost.builder()
+                .title(clubPostReq.getTitle())
+                .detailedDescription(clubPostReq.getDescription())
+                .url1(clubPostReq.getUrl1())
+                .url2(clubPostReq.getUrl2())
+                .user(user)
+                //.clubPostImages()
+                .build();
+        clubPostRepository.save(clubPost);
+    }
 }
--- src/main/java/depth/main/ideac/domain/club_post/presentation/ClubPostController.java
+++ src/main/java/depth/main/ideac/domain/club_post/presentation/ClubPostController.java
@@ -1,11 +1,43 @@
 package depth.main.ideac.domain.club_post.presentation;
 
+import depth.main.ideac.domain.club_post.application.ClubPostService;
+import depth.main.ideac.domain.club_post.dto.ClubPostReq;
+import depth.main.ideac.global.config.security.token.CurrentUser;
+import depth.main.ideac.global.config.security.token.UserPrincipal;
+import depth.main.ideac.global.payload.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/club-post")
+@RequestMapping("/api/club")
 public class ClubPostController {
+
+    private final ClubPostService clubPostService;
+
+    // 글 전체 조회
+
+    // 글 상세 조회
+
+    // 글 등록하기
+    @PostMapping("/register")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> createClubPost(@CurrentUser UserPrincipal userPrincipal,
+                                            @Valid @RequestBody ClubPostReq clubPostReq) {
+        clubPostService.createClubPost(userPrincipal.getId(), clubPostReq);
+        ApiResponse apiResponse = ApiResponse.builder()
+                .check(true)
+                .information("글이 등록되었습니다.")
+                .build();
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    // 글 수정하기
+    // 글 삭제하기
 }

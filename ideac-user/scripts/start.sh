5be4f7f8956e642c7ffacce33fb99e93e7b469cd
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2024-01-01 14:31:59 +0900

    Update start.sh

--- ideac-user/scripts/start.sh
+++ ideac-user/scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app/idea-user"
+PROJECT_ROOT="/home/ubuntu/app/ideac-user"
 JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"

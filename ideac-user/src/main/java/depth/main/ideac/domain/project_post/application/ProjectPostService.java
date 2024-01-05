f71594c23eb0c68beec38d209a2705bdc5aee628
ibaesuyeon <ibaesuyeon@Naver.com>
2024-01-05 22:10:09 +0900

    Feat: 프로젝트 게시 API 구현 #29

--- ideac-user/src/main/java/depth/main/ideac/domain/project_post/application/ProjectPostService.java
+++ ideac-user/src/main/java/depth/main/ideac/domain/project_post/application/ProjectPostService.java
@@ -1,12 +1,46 @@
 package depth.main.ideac.domain.project_post.application;
 
+import depth.main.ideac.domain.project_post.ProjectPost;
+import depth.main.ideac.domain.project_post.dto.request.PostProjectReq;
+import depth.main.ideac.domain.project_post.repository.ProjectPostRepository;
+import depth.main.ideac.domain.user.domain.User;
+import depth.main.ideac.domain.user.domain.repository.UserRepository;
+import depth.main.ideac.global.error.DefaultException;
+import depth.main.ideac.global.payload.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class ProjectPostService {
 
+    private final ProjectPostRepository projectPostRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void postProject(Long userId, PostProjectReq postProjectReq) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new DefaultException(ErrorCode.USER_NOT_FOUND));
+        if (!postProjectReq.isBooleanWeb() && !postProjectReq.isBooleanApp() && !postProjectReq.isBooleanAi()) {
+                throw new DefaultException(ErrorCode.INVALID_PARAMETER, "키워드는 하나 이상 표시해야 합니다.");
+        }
+        ProjectPost projectPost = ProjectPost.builder()
+                .title(postProjectReq.getTitle())
+                .simpleDescription(postProjectReq.getSimpleDescription())
+                .detailedDescription(postProjectReq.getDetailedDescription())
+                .teamInformation(postProjectReq.getTeamInformation())
+                .githubUrl(postProjectReq.getGithubUrl())
+                .webUrl(postProjectReq.getWebUrl())
+                .googlePlayUrl(postProjectReq.getGooglePlayUrl())
+                .booleanWeb(postProjectReq.isBooleanWeb())
+                .booleanApp(postProjectReq.isBooleanApp())
+                .booleanAi(postProjectReq.isBooleanAi())
+                .team(user.getOrganization())
+                .user(user)
+//                .projectPostView(null)
+//                .projectPostImages(postProjectReq.getProjectPostImages)
+                .build();
+        projectPostRepository.save(projectPost);
+    }
 }
--- ideac-user/src/main/java/depth/main/ideac/domain/project_post/presentation/ProjectPostController.java
+++ ideac-user/src/main/java/depth/main/ideac/domain/project_post/presentation/ProjectPostController.java
@@ -1,11 +1,27 @@
 package depth.main.ideac.domain.project_post.presentation;
 
+import depth.main.ideac.domain.project_post.application.ProjectPostService;
+import depth.main.ideac.domain.project_post.dto.request.PostProjectReq;
+import depth.main.ideac.global.config.security.token.CurrentUser;
+import depth.main.ideac.global.config.security.token.UserPrincipal;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/project-post")
+@RequiredArgsConstructor
 public class ProjectPostController {
+
+    private final ProjectPostService projectPostService;
+
+    @Operation(summary = "프로젝트 게시", description = "프로젝트 게시글을 생성하는 API입니다.")
+    @PostMapping("")
+    public ResponseEntity<?> postProject(@CurrentUser UserPrincipal userPrincipal,
+                                         @Valid @RequestBody PostProjectReq postProjectReq) {
+        projectPostService.postProject(userPrincipal.getId(), postProjectReq);
+        return ResponseEntity.ok().build();
+    }
 }

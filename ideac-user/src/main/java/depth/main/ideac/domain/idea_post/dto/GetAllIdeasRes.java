7b3049082f71bf6a58ba2909b918462ce48cd33a
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2024-01-11 04:32:42 +0900

    Feat: 아이디어 조회 시 응답 값 추가

--- ideac-user/src/main/java/depth/main/ideac/domain/idea_post/dto/GetAllIdeasRes.java
+++ ideac-user/src/main/java/depth/main/ideac/domain/idea_post/dto/GetAllIdeasRes.java
@@ -5,14 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 @AllArgsConstructor
 public class GetAllIdeasRes {
+    private Long id;
     private String title;
     @NotBlank(message = "내용이 입력되지 않았습니다.")
     private String simpleDescription;
     private String keyword;
     private String nickName;
     private String color;
+    private Long hits;
+    private LocalDateTime createdAt;
 }
